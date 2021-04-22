a7c4fb0833a141c9528a3401643a80ab6bc563cb
Adrian <adrian.asencio92@hotmail.com>
2021-04-22 16:51:43 +0200

    prueba

--- data.js
+++ data.js
@@ -4,7 +4,7 @@
 const climbers = [{
         name: "Sam",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat tellus in iaculis feugiat. Nullam sit amet massa metus. Duis in orci lectus. Praesent et erat nisl. Ut non eleifend odio. Fusce quis ultrices nisi. Suspendisse imperdiet lorem eu ligula finibus facilisis. Nullam sit amet orci viverra, iaculis ipsum sit amet, cursus nibh. Mauris semper, justo eget auctor vestibulum, sem erat bibendum turpis, at rhoncus mauris nisl vitae purus. Curabitur tempor mauris sit amet pulvinar faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi suscipit sed est ac finibus. Donec feugiat, felis eleifend congue posuere, ipsum libero fringilla ex, non blandit eros ipsum sit amet velit.",
-        age: Math.floor(Math.random() * 50),
+        age: 13,
         city: "Barcelona"
     },
     {
